39a6ed034bd1e9ecc9da90bef548726cb314875b
jendaaaaa <143932605+jendaaaaa@users.noreply.github.com>
2023-12-18 10:25:53 +0100

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 led.enable(false)
 let HORIZONTAL_SPEED = 80
 let WINCH_SPEED = 80
-let ROTATION_SPEED = 70
+let ROTATION_SPEED = 80
 let PRESSED = 0
 let RELEASED = 1
 let SERVO_STALL = 90
@@ -14,7 +14,7 @@ let left = 0
 let right = 0
 let forward = 0
 let backward = 0
-radio.setGroup(80)
+radio.setGroup(81)
 pins.setPull(DigitalPin.P13, PinPullMode.PullUp)
 pins.setPull(DigitalPin.P14, PinPullMode.PullUp)
 pins.setPull(DigitalPin.P15, PinPullMode.PullUp)
@@ -35,11 +35,11 @@ basic.forever(function () {
         rotor = SERVO_STALL
     }
     if (forward == PRESSED && backward == RELEASED) {
-        spool_inner = SERVO_STALL - HORIZONTAL_SPEED
-        spool_outer = SERVO_STALL + HORIZONTAL_SPEED
-    } else if (forward == RELEASED && backward == PRESSED) {
         spool_inner = SERVO_STALL + HORIZONTAL_SPEED
         spool_outer = SERVO_STALL - HORIZONTAL_SPEED
+    } else if (forward == RELEASED && backward == PRESSED) {
+        spool_inner = SERVO_STALL - HORIZONTAL_SPEED
+        spool_outer = SERVO_STALL + HORIZONTAL_SPEED
     } else if (forward == RELEASED && backward == RELEASED) {
         spool_inner = Math.map(hoist, 0, 1023, SERVO_STALL - WINCH_SPEED, SERVO_STALL + WINCH_SPEED)
         spool_outer = spool_inner
@@ -52,17 +52,25 @@ basic.forever(function () {
     radio.sendValue("outer", spool_outer)
     radio.sendValue("gripper", gripper)
 })
+
 control.onEvent(EventBusSource.MICROBIT_ID_BUTTON_A, EventBusValue.MICROBIT_EVT_ANY, function () {
     if (control.eventValue() == EventBusValue.MICROBIT_BUTTON_EVT_DOWN) {
         gripper = 1
-    } else {
-        gripper = 0
     }
+    // if (control.eventValue() == EventBusValue.MICROBIT_BUTTON_EVT_DOWN) {
+    //     gripper = 1
+    // } else {
+    //     gripper = 0
+    // }
 })
+
 control.onEvent(EventBusSource.MICROBIT_ID_BUTTON_B, EventBusValue.MICROBIT_EVT_ANY, function () {
     if (control.eventValue() == EventBusValue.MICROBIT_BUTTON_EVT_DOWN) {
-        gripper = 1
-    } else {
         gripper = 0
     }
+    // if (control.eventValue() == EventBusValue.MICROBIT_BUTTON_EVT_DOWN) {
+    //     gripper = 1
+    // } else {
+    //     gripper = 0
+    // }
 })
